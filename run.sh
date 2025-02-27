fae3508454ddbe6518b8fae8cfa250fc15ce9370
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-02-27 11:42:35 +1000

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Usage: ./wake_devices.sh
+# chmod +x wake_devices.sh
+# ./wake_devices.sh
+
+# Run the wakeonlan.py script with the specified MAC addresses
+./wakeonlan.py 90:09:d0:0c:47:95 74:56:3c:71:82:03
